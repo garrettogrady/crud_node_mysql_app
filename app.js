8e14eaace6736f733c8cfee27b20e358067b3e11
Garrett O'Grady <ogradyg1@udayton.edu>
2018-11-01 21:21:18 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 const {getHomePage} = require('./routes/index');
 const {addEventPage, addEvent, deleteEvent, editEvent, editEventPage} = require('./routes/event');
-const {addUserPage, addUser, deleteUser, editUser, editUserPage} = require('./routes/login');
+const {addUserPage, addUser, deleteUser, editUser, editUserPage, addloginPage, loginUser, logoutUser, showUserPage} = require('./routes/login');
 
 const port = 5000;
 
@@ -41,14 +41,22 @@ app.get('/', getHomePage);
 app.get('/add_event', addEventPage);
 app.get('/edit_event/:id', editEventPage);
 app.get('/delete_event/:id', deleteEvent);
+
 app.post('/add_event', addEvent);
 app.post('/edit_event/:id', editEvent);
 
 app.get('/add_user', addUserPage);
 app.get('/edit_user/:id', editUserPage);
 app.get('/delete_user/:id', deleteUser);
+app.get('/login_user', addloginPage);
+app.get('/logout_user', logoutUser);
+app.get('/show_user/:id', showUserPage);
+
+
 app.post('/add_user', addUser);
 app.post('/edit_user/:id', editUser);
+app.post('/login_user', loginUser);
+
 
 
 
