f9a9726a4937bb9bbf62256221602bdeb556887e
bilgrie2 <bilgrie2@illinois.edu>
2018-11-24 14:15:49 -0600

    added tables for upcoming, past and user's events on the homepage with sql queries

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,73 @@
 module.exports = {
 	getHomePage: (req, res) => {
-		let query = "SELECT * FROM `events` e Join `users` u ON e.host_id = u.id ORDER BY event_id ASC";
+        
+        if(!global.userSignedIn){
+            res.redirect('/login_user');
+        }
+        
+        var async = require('async');
+        
+        let today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1;
+        var yyyy = today.getFullYear();
+        if(dd<10) dd='0'+dd;
+        if(mm<10) mm='0'+mm;
+        
+        today = mm+'/'+dd+'/'+yyyy;
+        
+        current_user_id = global.userSignedIn;
 
-		//execute query
-		db.query(query, (err, result) => {
+		//execute queries
+        async.parallel([
+            
+            function(callback){
+                let future_events_query = "SELECT * FROM `events` e Join `users` u ON e.host_id = u.id Join `invited` i ON i.event_id = e.event_id WHERE e.event_date >= '" + today + "' AND i.guest_id = " + current_user_id;
+                db.query(future_events_query, function(err, futureEvents){
+                    if(err){
+                        return callback(err);
+                    }
+                    return callback(null, futureEvents);
+                });
+            },
+            function(callback){
+                let past_events_query = "SELECT * FROM `events` e Join `users` u ON e.host_id = u.id Join `invited` i ON i.event_id = e.event_id WHERE e.event_date < '" + today + "' AND i.guest_id = " + current_user_id;
+                
+                db.query(past_events_query, function(err, pastEvents){
+                    if(err){
+                        return callback(err);
+                    }
+                    return callback(null, pastEvents);
+                });
+            },
+            function(callback){
+                let user_events_query = "SELECT * FROM `events` e Join `users` u ON e.host_id = u.id WHERE e.host_id = " + current_user_id;
+                
+                db.query(user_events_query, function(err, userEvents){
+                    if(err){
+                        return callback(err);
+                    }
+                    return callback(null, userEvents);
+                });
+            }
+            
+        ], function(error, callbackResults){
+            if(error){
+                console.log(error);
+            } else {
+                console.log(callbackResults[0]); //upcoming events
+                console.log(callbackResults[1]); //past events
+                res.render('index.ejs', {
+                    title: "Welcome to whatslit | View events",
+                    event: callbackResults,
+                    currentUser: global.userSignedIn
+                });
+            }
+        });
+        /*
+		db.query(past_events_query, (err, result) => {
             if (err) {
-                res.redirect('/');
+                console.log(err);
             }
             res.render('index.ejs', {
                 title: "Welcome to whatslit | View events"
@@ -13,5 +75,8 @@ module.exports = {
                 ,currentUser: global.userSignedIn
             });
         });
-	},
+        */
+        
+        
+    }
 };
