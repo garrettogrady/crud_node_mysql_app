c94fca78badf3d0a7c35a934a43da8098be41f25
Garrett O'Grady <ogradyg1@udayton.edu>
2018-12-03 15:03:56 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -79,7 +79,7 @@ module.exports = {
                     "DROP TABLE IF EXISTS InvitedGuests;" +
                     "CREATE TEMPORARY TABLE InvitedGuests AS SELECT invited.guest_id FROM UserEvents NATURAL JOIN invited; " +
                     "DROP TABLE IF EXISTS GuestEvents;" +
-                    "CREATE TEMPORARY TABLE GuestEvents AS SELECT DISTINCT Invited.event_id FROM InvitedGuests NATURAL JOIN invited; " +
+                    "CREATE TEMPORARY TABLE GuestEvents AS SELECT DISTINCT invited.event_id FROM InvitedGuests NATURAL JOIN invited; " +
                     "SELECT * FROM UserInvites NATURAL JOIN events WHERE EXISTS (SELECT event_id FROM GuestEvents);";
 
                 db.query(suggested_events_query, function(err, suggestedEvents){
