416d02d094db01d0fb2296569b36fc388b0fb8cc
Garrett O'Grady <ogradyg1@udayton.edu>
2018-12-03 14:34:07 -0600

    Update bones.sql

--- bones.sql
+++ bones.sql
@@ -1,1 +1,107 @@
 
+
+START TRANSACTION;
+
+CREATE TABLE `events` (
+  `event_id` int(5) NOT NULL,
+  `event_name` varchar(255) NOT NULL,
+  `event_address` varchar(255) NOT NULL,
+  `event_date` varchar(255) NOT NULL,
+  `event_time` varchar(255) NOT NULL,
+  `event_level` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `host_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `invited` (
+  `event_id` int(11) NOT NULL,
+  `guest_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `reviews` (
+  `review_id` int(11) NOT NULL,
+  `event` int(11) NOT NULL,
+  `reviewee_id` int(11) DEFAULT NULL,
+  `review_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `rating` int(11) DEFAULT NULL,
+  `comment` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`),
+  ADD KEY `host_id` (`host_id`);
+
+--
+-- Indexes for table `invited`
+--
+ALTER TABLE `invited`
+  ADD PRIMARY KEY (`event_id`,`guest_id`),
+  ADD KEY `fk_guest_id` (`guest_id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`review_id`),
+  ADD KEY `fk_reviewee` (`reviewee_id`),
+  ADD KEY `fk_event` (`event`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `events`
+  MODIFY `event_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `host_event_relation` FOREIGN KEY (`host_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `invited`
+--
+ALTER TABLE `invited`
+  ADD CONSTRAINT `fk_event_id` FOREIGN KEY (`event_id`) REFERENCES `events` (`event_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_guest_id` FOREIGN KEY (`guest_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `fk_event` FOREIGN KEY (`event`) REFERENCES `events` (`event_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_reviewee` FOREIGN KEY (`reviewee_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
+COMMIT;
